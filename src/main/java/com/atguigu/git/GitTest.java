0eef7c1ff158d6222e3bd3032d9cc1cb4c732b43
Francis <Francis@freedom.com>
2021-07-23 15:36:31 +0800

    first  commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+
+/*
+ * @author TY
+ * @date 2021/7/23 - 15:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
